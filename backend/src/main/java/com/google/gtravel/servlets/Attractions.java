b403965088f1814f775546f17d8f94fa9e84e472
AlexandraBulaceanu <alemsb29@gmail.com>
2020-08-21 13:53:48 +0000

    Update the structure of the project. I have integrated the fronted and the backend. Also, I have integrated the Maps API with React. The features are: search for a city and a list of attractions will appear on the screen. The dynamic version of the itinerary can be tested(it works by getting the locations from the url). Also, I connected by using React Route the landing page and the itinerary page.

---
+++ backend/src/main/java/com/google/gtravel/servlets/Attractions.java
@@ -0,0 +1,76 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gtravel.servlets;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+@WebServlet("/api/attractions")
+public class Attractions extends HttpServlet {
+  SecretKeys secrets = new SecretKeys();
+
+  private String API_KEY = secrets.getGCP_API_KEY();
+  private final CloseableHttpClient httpClient = HttpClients.createDefault();
+
+  public String getRequest(String url) throws IOException {
+    String output = "";
+    HttpGet req = new HttpGet(url);
+
+    try (CloseableHttpResponse res = httpClient.execute(req)) {
+
+      HttpEntity entity = res.getEntity();
+      Header headers = entity.getContentType();
+
+      if (entity != null) {
+        String result = EntityUtils.toString(entity);
+        output += result;
+      }
+    }
+    return output;
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    String latitude = request.getParameter("lat");
+    String longitude = request.getParameter("long");
+    // For Munich
+    // lat = 48.1351
+    // long = 11.5820
+    // Example Usage:
+    // example.com/attractions?lat=48.1351&long=11.5820
+
+    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+    url += latitude;
+    url += ",";
+    url += longitude;
+
+    url += "&radius=10000&type=tourist_attraction&key=" + API_KEY;
+    String result = getRequest(url);
+    response.setContentType("application/json");
+    response.getWriter().println(result);
+    response.addHeader("Access-Control-Allow-Origin", "*");
+  }
+}
---
+++ frontend/src/Map.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ITINERARY_KEY from "./configMap";
+import './Itinerary.css';
+
+class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.onScriptLoad = this.onScriptLoad.bind(this)
+    }
+
+    onScriptLoad() {
+        const map = new window.google.maps.Map(
+        document.getElementById(this.props.id),
+        this.props.options);
+
+        let query = window.location.search.substring(1);
+        let vars = query.split("&");
+        let query_string = {};
+
+        for (var i = 0; i < vars.length; i++) {
+
+            const pair = vars[i].split("=");
+            const key = decodeURIComponent(pair[0]);
+            const value = decodeURIComponent(pair[1]);
+            query_string[key] = value;
+        }
+        let urlParameters = query_string;
+        const centerPoint = urlParameters.locations.split('*')[0];
+
+        const centerLat = parseFloat(centerPoint.split(',')[0]);
+        const centerLong = parseFloat(centerPoint.split(',')[1]);
+
+        const directionsService = new window.google.maps.DirectionsService();
+        const directionsRenderer = new window.google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(map);
+        
+        let waypts = [];
+        query = window.location.search.substring(1);
+        vars = query.split("&");
+        query_string = {};
+
+        for (var i = 0; i < vars.length; i++) {
+
+            const pair = vars[i].split("=");
+            const key = decodeURIComponent(pair[0]);
+            const value = decodeURIComponent(pair[1]);
+            query_string[key] = value;
+
+        }
+        urlParameters = query_string;
+        const locations = urlParameters.locations.split('*');
+
+
+        const start = locations[0];
+        const finish = locations[locations.length - 1];
+
+        for (let i = 1; i < locations.length - 1; i++) {
+            waypts.push({
+                location: locations[i],
+                stopover: true,
+            });
+        }
+
+
+        directionsService.route(
+            {
+                origin: start,
+                destination: finish,
+                waypoints: waypts,
+                optimizeWaypoints: true,
+                travelMode: window.google.maps.TravelMode.DRIVING,
+            },
+            (response, status) => {
+                if (status === 'OK') {
+                    directionsRenderer.setDirections(response);
+                    const route = response.routes[0];
+
+                    const summaryPanel = document.getElementById('directions-panel');
+                    summaryPanel.innerHTML = '';
+
+                    for (let i = 0; i < route.legs.length; i++) {
+                        const routeSegment = i + 1;
+                        summaryPanel.innerHTML +=
+                            '<b>Route Segment: ' + routeSegment + '</b><br>';
+                        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                        summaryPanel.innerHTML +=
+                            route.legs[i].distance.text + '<br><br>';
+                    }
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            },
+        );
+
+
+        this.props.onMapLoad(map)
+    }
+
+    componentDidMount() {
+        if (!window.google) {
+            const key = Object.values(ITINERARY_KEY)[0];
+            console.log(key);
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.src = `https://maps.google.com/maps/api/js?key=AIzaSyDNZQ2ywFAOpSWjJS33BWKNfTtB4jSskAs`;
+            document.head.appendChild(s);
+            s.addEventListener('load', e => {
+                this.onScriptLoad()
+            })
+        } else {
+            this.onScriptLoad()
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="left-panel">
+                    <div id="directions-panel"></div>
+                </div>
+                <h1 id="title">My Itinerary</h1>
+                <div style={{ width: 1000, height: 800, float: 'left', position: 'absolute', right: 60 }} id={this.props.id}></div>
+            </div>
+        );
+    }
+}
+
+export default Map
