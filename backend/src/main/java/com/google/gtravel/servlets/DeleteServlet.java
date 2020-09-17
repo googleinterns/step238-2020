a3f29ea0b3b1b9aa1532bbf53cb5433d52cb5962
AlexandraBulaceanu <alemsb29@gmail.com>
2020-09-17 15:44:32 +0000

    Conflict7

--- backend/src/main/java/com/google/gtravel/servlets/DeleteServlet.java
+++ backend/src/main/java/com/google/gtravel/servlets/DeleteServlet.java
@@ -1,4 +1,4 @@
-package com.google.gtravel.servlets;
+package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -9,27 +9,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 
-/** Servlet responsible for deleting  comments. */
+/** Servlet responsible for deleting comments. */
 @WebServlet("/api/delete")
 public class DeleteServlet extends HttpServlet {
 
   @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-    doPost(request, response);
-}
-
-  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-    long id = Long.parseLong(request.getParameter("id"));
+    String id = request.getParameter("tripID");
 
     Key tripEntityKey = KeyFactory.createKey("Trip", id);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.delete(tripEntityKey);
     response.sendRedirect("/");
-
   }
 }
